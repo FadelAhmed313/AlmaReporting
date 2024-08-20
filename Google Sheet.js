62fee5398c51640e59cc8fc22d4408505e12df91
FadelAhmed313 <146953610+FadelAhmed313@users.noreply.github.com>
2024-08-20 12:23:28 +0400

    Update Google Sheet.js

--- Google Sheet.js
+++ Google Sheet.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbz5wIOaIRcpAfyTWT4WsFBtZ2n57OCmVBSDdJU6HQllCraDLYHD8jB66A1eqAg36-F9/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzTfcxMnqTJZ8Ztc9WYjB_GxRu9spTGM0ACGpk_OKyw8p7dvUnY0BIjF71yQIoOK3p8/exec'
 
 const form = document.forms['contact-form']
 
